408017f1c367e493715f6588195b4355a10ce8cb
MakFil <makfil1177@gmail.com>
2021-02-24 16:44:05 +0200

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  str: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    // throw new CustomError('Not implemented');
+    let x = this.str.slice();
+    this.str.length = 0;
+    return x.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = "") {
+    // throw new CustomError('Not implemented');
+    let r = this.str.slice();
+    let val = value;
+    if (typeof value === "function") {
+      let v = value.toString().split("");
+      v.splice(v.indexOf(" "), 1);
+      val = v.join("");
+    }
+
+    this.str.push(`( ${val} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    // throw new CustomError('Not implemented');
+    if (
+      typeof position !== "number" ||
+      position - 1 > this.str.length ||
+      position < 0 ||
+      !Number.isInteger(position)
+    ) {
+      this.str.length = 0;
+      throw new Error("Error");
+    }
+    this.str.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    // throw new CustomError('Not implemented');
+    this.str.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    // throw new CustomError('Not implemented');
+    let x = this.str.slice();
+    this.str.length = 0;
+    return x.join("~~");
+  },
 };
 
 module.exports = chainMaker;
